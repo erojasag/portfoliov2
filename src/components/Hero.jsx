a8df87e983bb85d693579ac0fb308278f2ee3721
Emanuel Rojas <eroaguero01@gmail.com>
2024-03-13 00:25:00 -0600

    fix on the info

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,24 @@ function Hero() {
   return (
     <section className="flex flex-col justify-center items-center">
       <Logo />
-      <span className="uppercase text-secondary bg-primary px-4 py-1 mb-20 font-medium rounded">
+      <span className="uppercase text-secondary bg-primary px-4 py-1 mb-10 font-medium rounded">
         Software developer
       </span>
+      <div className="flex flex-row justify-between items-center pb-10">
+        <ul className="text-center">
+          <li>
+            Email:
+            <a className="text-primary" href="mailto: emanuel@emanuelrojas.com">
+              {' '}
+              emanuel@emanuelrojas.com
+            </a>
+          </li>
+          <li>
+            Mobile: <span className="text-primary">+506 8866-7456</span>
+          </li>
+          <li className="text-primary">Alajuela, Costa Rica</li>
+        </ul>
+      </div>
     </section>
   );
 }
