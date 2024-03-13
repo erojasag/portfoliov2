85189d2326df4b56f643793def62ce67b997c1d6
Emanuel Rojas <eroaguero01@gmail.com>
2024-03-13 00:11:36 -0600

    correction on image

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,7 +6,7 @@ function AboutMe() {
       <div className="max-w-sm w-full lg:max-w-full lg:flex rounded">
         <img
           src={img}
-          className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+          className="lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
           title="emanuel snowboard"
         ></img>
         <div className="border-r border-b border-l border-primary lg:border-l-0 lg:border-t lg:border-primary rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
